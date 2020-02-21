62dd17c479febc8cb3290e8326ade231ab8f3bb0
Jake Cannon <jakecan13@gmail.com>
2020-02-21 13:31:51 -0500

    add reverse_sentence function

--- reverse.py
+++ reverse.py
@@ -5,3 +5,22 @@ def reverse_string(word):
 	result = word[::-1]
 
 	return result
+
+def reverse_sentence(sentence):
+
+	# split the sentence up on white spaces
+	tokens = sentence.split(" ")
+
+	# reverse each word in the array
+	reversed_words = [reverse_string(x) for x in tokens]
+
+	# reverse entire array contents 
+	result_array = reversed_words[::-1]
+
+	# iterate throurgh result array and concatenate strings
+	result_string = ' '.join(word for word in result_array)
+
+	return result_string
+
+reverse_sentence("hello world")
+
--- reverse_test.py
+++ reverse_test.py
@@ -1,11 +1,16 @@
 import unittest
 from reverse import reverse_string
+from reverse import reverse_sentence
+
 
 class stringTest(unittest.TestCase):
 
-	def test_reverse(self):
+	def test_reverse_string(self):
 		self.assertEqual(reverse_string('dog'),'god')
+		
 
+	def test_reverse_sentence(self):
+		self.assertEqual(reverse_sentence('hello world'), 'dlrow olleh')
 
 
 if __name__ == '__main__':
