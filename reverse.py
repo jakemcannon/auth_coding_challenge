e6e424fca1b12f090dcb4a866163014604e263c0
Jake Cannon <jakecan13@gmail.com>
2020-02-21 16:06:13 -0500

    implement CLI with argparse

--- reverse.py
+++ reverse.py
@@ -1,3 +1,4 @@
+import argparse
 
 def reverse_string(word):
 
@@ -22,5 +23,36 @@ def reverse_sentence(sentence):
 
 	return result_string
 
-reverse_sentence("hello world")
 
+def main():
+
+	parser = argparse.ArgumentParser(description='Reverse strings(s) program')
+	parser.add_argument("string", help="Enter the string you would like to reverse")
+	parser.add_argument("-r", "--r", help="Select this option to reverse a single word", action="store_true")
+	parser.add_argument("-w", "--w", help="Select this option to reverse a string word for word", action="store_true")
+	parser.add_argument("-i", "--input", help="Select this option to reverse the contents of a file")
+
+	args = parser.parse_args()
+
+	# reverse single word located in input file 
+	if args.r and args.input:
+		with open(args.input, 'r') as f:
+			result = reverse_string(f.read())
+			print(result)
+	# reverse string of words located in input file
+	elif args.w and args.input:
+		with open(args.input, 'r') as f:
+			result =reverse_sentence(f.read())
+			print(result)
+	# reverse single word from command line argument
+	elif args.r:
+		result = reverse_string(args.string)
+		print(result)
+	# reverse string of words from command line argument
+	elif args.w:
+		result = reverse_sentence(args.string)
+		print(result)
+
+
+if __name__ == '__main__':
+	main()
--- reverse_test.py
+++ reverse_test.py
@@ -1,4 +1,5 @@
 import unittest
+import argparse
 from reverse import reverse_string
 from reverse import reverse_sentence
 
@@ -7,7 +8,7 @@ class stringTest(unittest.TestCase):
 
 	def test_reverse_string(self):
 		self.assertEqual(reverse_string('dog'),'god')
-		
+
 
 	def test_reverse_sentence(self):
 		self.assertEqual(reverse_sentence('hello world'), 'dlrow olleh')
